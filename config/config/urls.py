0839f0fc0ea658ead6de8b57f0c6b749d8b0feb9
Asgar Ansari <asabreenasgar@gmail.com>
2026-02-27 17:49:26 +0530

    fixed url structure properly

--- config/config/urls.py
+++ config/config/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
-from . import views
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -16,7 +15,6 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
     path('api/', include('boutique.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
