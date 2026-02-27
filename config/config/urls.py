898a4028e12195f1126dd5f349125215bc9a3230
Asgar Ansari <asabreenasgar@gmail.com>
2026-02-27 13:40:06 +0530

    remove temp admin route

--- config/config/urls.py
+++ config/config/urls.py
@@ -4,7 +4,6 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from . import views
-from .views import create_admin
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -21,5 +20,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('boutique.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
-    path("create-admin/", create_admin)
 ]
--- config/config/views.py
+++ config/config/views.py
@@ -2,13 +2,3 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Mariyam Boutique is Live 🚀")
-
-from django.contrib.auth import get_user_model
-from django.http import HttpResponse
-
-def create_admin(request):
-    User = get_user_model()
-    if not User.objects.filter(username="asgar").exists():
-        User.objects.create_superuser("asgar", "asgar@email.com", "Asgar@123")
-        return HttpResponse("Superuser created")
-    return HttpResponse("Already exists")
