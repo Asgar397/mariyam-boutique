a682fb023e91badd0a4c2403b14f6a4ec2ad90f2
Asgar Ansari <asabreenasgar@gmail.com>
2026-03-05 11:53:01 +0530

    add image field

--- boutique/models.py
+++ boutique/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Service(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    image = models.ImageField(upload_to='service_images/', null=True, blank=True)
 
     def __str__(self):
         return self.title
-
 
 class Customer(models.Model):
     name = models.CharField(max_length=100)
