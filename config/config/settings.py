720c23f8431b0ea89ea76a2b1547abc15860aaf3
Asgar Ansari <asabreenasgar@gmail.com>
2026-03-05 10:14:40 +0530

    create admin auto

--- config/config/settings.py
+++ config/config/settings.py
@@ -132,3 +132,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+from django.contrib.auth.models import User
+
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@gmail.com", "admin123")
